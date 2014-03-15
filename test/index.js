48cf2a0259332626f1991cb5abbab9e2cdaf3cf4
Matti Savolainen <matti@applifier.com>
2014-03-15 08:35:44 +0200

    run the exported test suite in series instead of parallel as they may use the same underlaying data store

--- test/index.js
+++ test/index.js
@@ -1,11 +1,9 @@
-var _ = require('lodash');
 /**
  * Allow this test suite to be used on custom implementations of db, model and collection.
  */
+var _ = require('lodash');
 exports.shouldImplementDb = function (done) {
-  var modelTests = require('./test.model.js');
-  var collectionTests = require('./test.collection.js');
-  var d = _.after(2, done);
-  collectionTests(d);
-  modelTests(d);
+  require('./test.model.js')(function() {
+    require('./test.collection.js')(done);
+  });
 };
