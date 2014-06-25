543103f224463c1d1d56164f529636f14bcc1548
Mikko Lehtinen <mikko+gh@applifier.com>
2014-06-25 10:41:24 +0300

    Fixed error inheritance

--- test/interface.test.js
+++ test/interface.test.js
@@ -13,5 +13,8 @@ describe('backbone-db interface tests', function () {
   it('should init a NotFound Error', function() {
     var err = new Db.errors.NotFoundError('Foo');
     assert.equal(err.statusCode, 404);
+    assert.equal(err.message, 'Foo');
+    assert(err instanceof Error);
+    assert(err instanceof Db.errors.NotFoundError);
   });
 });
